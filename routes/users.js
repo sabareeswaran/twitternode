346bc0d9fe4be7cef47a73cdb18d25425fce5a76
Vivekanandan <srivivek13.sv@gmail.com>
2017-12-26 01:41:44 +0530

    ++ Access Token

--- routes/users.js
+++ routes/users.js
@@ -45,6 +45,41 @@ request({
 });
 
 router.post('/',function(req,res,next){
-	res.json(req.body);
+	var oauth_verifier=req.body.oauth_verifier;
+	var oauth_token=req.body.oauth_token;
+	const oauth = OAuth({
+		consumer: {
+			key: 'ZoNxViPw2sHSDKhYeBXxKqZvI',
+			secret: 'kfHbyueFbpgRUbHVPgJLcjrlo1CyZL7nhpIAZi4ExXe59IOChT'
+		},
+		signature_method: 'HMAC-SHA1',
+		hash_function(base_string, key) {
+			return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+		}
+	});
+ 
+	const request_data = {
+	  url: 'https://api.twitter.com/oauth/access_token',
+	  method: 'POST',
+	  data: 'oauth_verifier='+oauth_verifier
+	};
+ 
+	/* Note: The token is optional for some requests
+	 */
+	const token = {
+	  key:'oauth_token'
+	};
+	request({
+		url: request_data.url,
+		method: request_data.method,
+		form: request_data.data,
+		headers: oauth.toHeader(oauth.authorize(request_data))
+	}, function(error, response, body) {
+		if(error)
+		res.json(error);
+		else{
+			res.json(response);
+		}
+	});
 });
 module.exports = router;
